62d0f8f4e043a3f7d90ba4547b6012dd5ba6795d
Abhishek Nayak <abhishek2002gtmail@gmail.com>
2024-05-12 11:30:53 +0530

    Used Postgresql Database

--- app/main.py
+++ app/main.py
@@ -1,56 +1,100 @@
 from fastapi import FastAPI , Response , HTTPException ,status
-from typing import Optional 
+from typing import Optional , Union
 from pydantic import BaseModel 
 from random import randrange
+import psycopg2
+from psycopg2.extras import RealDictCursor
 
 class DataModel(BaseModel):
     title : str 
     Price : int
 
+class database_entry(BaseModel):
+    name : str 
+    quantity : int 
+    on_sale : Optional[bool] 
+    price : int 
+
+class update_data(BaseModel):
+    column : str 
+    value : Optional[Union[str, int]]
+
+
 app = FastAPI()
 
 data_collection = [{"title" : "The Haunted Ship" , "Price" : 260 , "id" : 24242},{"title" : "The Runner" , "Price" : 1260 ,"id" : 23232},{"title" : "Laapta Ladies" , "Price" : 660 , "id" : 1212}] 
 
+# Database Connection 
+try:
+
+    while True:
+
+        conn = psycopg2.connect(host="localhost",user="postgres",password="1@12@23@34@4" , database="postgres" , cursor_factory=RealDictCursor)
+
+        cursor = conn.cursor()
+
+        print("Connection Successsfull")
+        break
+except Exception as e:
+    print(e)
 @app.get("/posts")
 def get_all_posts():
-    return {"Data" : data_collection} 
+        cursor.execute("""
+                    SELECT * from products 
+                    """)
+        
+        result = cursor.fetchall()
+        print(result)
+        return {"Data" : result} 
 
 @app.post("/posts")
-def make_a_post(data : DataModel , response : Response):
+def make_a_post(data : database_entry , response : Response):
     try:
-        id = randrange(1,100000000)
         data = data.dict() 
-        data['id'] = id 
-        data_collection.append(data) 
+        print(data)
+        result = cursor.execute("""INSERT INTO products (name , quantity , on_sale , price) VALUES(%s,%s,%s,%s) returning *""",(data['name'] , data['quantity'] , data['on_sale'] , data['price']))
+        result = cursor.fetchone()
+        conn.commit()
         response.status_code = status.HTTP_201_CREATED 
-        return {"Data":data_collection} 
-    except:
+        print(result)
+        return {"Data":result} 
+    except Exception as e:
+        print(e)
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST , detail= "Post not created")
 
 @app.get("/posts/{id}")
 def get_particular_posts(id : int):
-    for i in data_collection:
-        if i['id'] == id : 
-            return i 
-    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST , detail= "NO such post") 
+    try:
+        cursor.execute("""SELECT * from products where product_id = %s""" , (id , )) 
+        data = cursor.fetchone() 
+        return {"Data" : data} 
+    except Exception as e:
+        return HTTPException(status_code=status.HTTP_400_BAD_REQUEST , detail = str(e))
 
 @app.put("/posts/{id}")
-def update_posts(id : int , data : DataModel):
-    data = data.dict()
-    for i in data_collection:
-        if i['id'] == id:
-            i['title'] = data['title']
-            i['Price'] = data['Price'] 
-            return {"New Data" : get_particular_posts(id)} 
-    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST , details = "NO such Post exists")
+def update_posts(id : int , data : update_data):
+    try:
+        data = data.dict()
+        result = cursor.execute(f"""UPDATE products SET {data['column']} = %s WHERE product_id = %s RETURNING *""" , (data['value'] , id))
+        result = cursor.fetchone()
+        conn.commit()
+        print("Data Updated Successfully") 
+        return {"Data" : result}
+    
+    except Exception as e:
+        return HTTPException(status_code=status.HTTP_400_BAD_REQUEST , detail=str(e))
+    
 
 @app.delete("/posts/{id}" , status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id : int):
-    for i in data_collection:
-        if i['id'] == id:
-            data_collection.remove(i) 
-            return {"Response":status.HTTP_204_NO_CONTENT} 
-    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST , details= "No such Post")
+    try:
+        cursor.execute("""DELETE FROM products where product_id = %s returning *""" , (id ,))
+        result = cursor.fetchone()
+        conn.commit()
+        print("Data Deleted Successfully")
+        return {"Data" : result}
+    except Exception as e:
+        return HTTPException(status_code=status.HTTP_400_BAD_REQUEST , detail=str(e))
         
 
 
